412e9df28fee088ab0a153556d979de26aca96d1
陈随易 <chenbimo@gmail.com>
2025-02-04 21:51:07 +0800

    i18n配置调整

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -2,6 +2,8 @@ import { createI18n } from 'vue-i18n';
 import { yiteMessages } from 'virtual:yite-messages';
 
 const $I18n = createI18n({
+    legacy: false,
+    fallbackLocale: 'zh',
     locale: 'zh',
     messages: await yiteMessages()
 });
